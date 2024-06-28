1132dddf8016f2bbe6dc527b91494fff9f498d2a
SurajMotwani1306 <suraj.motwani1306@gmail.com>
2024-06-28 10:25:48 -0700

    Intial App

--- App.tsx
+++ App.tsx
@@ -1,118 +1,96 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { SafeAreaView, ScrollView, StyleSheet, View } from 'react-native'
+import React, { useState } from 'react';
+import StylishList from 'react-native-stylishlist';
+import BottomSheet from './src/BottomSheet';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const StylishLst = () => {
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  const pressEventTrigger = (id : number, data: any) => {
+    console.warn(id);
+    // console.warn(data)
+  }
+  
+  const updateProps = (obj:any) => {
+    setProps(obj);
+  }
+  
+  let dataList = [
+    {
+      leftImageUrl: 'https://cdn-icons-png.flaticon.com/256/3001/3001785.png',
+      leftImageBackgroundColor: '#00D8B1',
+      itemTitle: 'General',
+      itemDescription: 'Lorem ipsum dolor sit amet',
+      // itemColorCircle: ['#EAEAEA', 'black', 'blue'],
+      onPress: pressEventTrigger,
+      // checkBoxStatus: true,
+      rightSideText: '$26',
+      rightSideTextColor: 'blue'
+    },
+    {
+      leftImageUrl: 'https://cdn-icons-png.flaticon.com/256/3001/3001785.png',
+      leftImageBackgroundColor: '#00D8B1',
+      itemTitle: 'Privacy & security',
+      itemDescription: 'Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet',
+      // itemColorCircle: ['orange', 'cyan'],
+      onPress: pressEventTrigger,
+      // checkBoxStatus: true,
+      rightSideText: '$20',
+      rightSideTextColor: 'blue'
+    },
+    {
+      leftImageUrl: 'https://cdn-icons-png.flaticon.com/256/3001/3001785.png',
+      leftImageBackgroundColor: '#00D8B1',
+      itemTitle: 'Notification',
+      itemDescription: 'Lorem ipsum dolor sit amet',
+      // itemColorCircle: ['green', 'orange'],
+      onPress: pressEventTrigger,
+      // checkBoxStatus: false,
+      rightSideText: '$45',
+      rightSideTextColor: 'blue'
+    },
+    {
+      leftImageUrl: 'https://cdn-icons-png.flaticon.com/256/3001/3001785.png',
+      leftImageBackgroundColor: '#00D8B1',
+      itemTitle: 'Account',
+      itemDescription: 'Lorem ipsum dolor sit amet',
+      // itemColorCircle: ['green', 'orange'],
+      onPress: pressEventTrigger,
+      // checkBoxStatus: false,
+      rightSideText: '$45',
+      rightSideTextColor: 'blue'
+    }
+  ];
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  let [props, setProps] = useState({ 
+    heading: 'Settings',
+    headingColor: 'black', 
+    headingSize: 14, 
+    // checkboxContainsList: false,
+    leftImageUrlFlag: true,
+    belowLine: true,
+    itemBoxBorder: false,
+    itemBoxBorderLeftHighlight: false,
+    itemBoxBorderLeftHighlightColor: 'blue',
+    rightArrowVisibility: true,
+    rightSideTextFlag: true,
+    rightSideIcon: 'https://cdn-icons-png.flaticon.com/256/3001/3001785.png',
+    dataList: dataList,
+    updateProps: updateProps
+  });
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <StylishList {...props}/>
+          <BottomSheet {...props}/>
         </View>
       </ScrollView>
     </SafeAreaView>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+export default StylishLst
 
-export default App;
+const styles = StyleSheet.create({
+})
---
+++ src/BottomSheet.tsx
@@ -0,0 +1,418 @@
+import { Alert, Button, Modal, Pressable, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import CheckBox from '@react-native-community/checkbox';
+
+const BottomSheet = (props: any) => {
+    //Manage States
+    const [modalVisible, setModalVisible] = useState(false)
+
+    const [heading, setHeading] = useState(props.heading)
+    const [headingColor, setHeadingColor] = useState(props.headingColor)
+    const [headingSize, setHeadingSize] = useState(props.headingSize)
+    const [leftImageUrlFlag, setleftImageUrlFlag] = useState(props.leftImageUrlFlag)
+    const [belowLine, setbelowLine] = useState(props.belowLine)
+    const [itemBoxBorder, setItemBoxBorder] = useState(props.itemBoxBorder)
+    const [itemBoxBorderLeftHighlight, setItemBoxBorderLeftHighlight] = useState(props.itemBoxBorderLeftHighlight)
+    const [itemBoxBorderLeftHighlightColor, setItemBoxBorderLeftHighlightColor] = useState(props.itemBoxBorderLeftHighlightColor)
+    const [rightArrowVisibility, setRightArrowVisibility] = useState(props.rightArrowVisibility)
+    const [rightSideTextFlag, setRightSideTextFlag] = useState(props.rightSideTextFlag)
+    const [rightSideIcon, setRightSideIcon] = useState(props.rightSideIcon)
+
+
+
+    const [leftImageUrl, setLeftImageUrl] = useState(props.dataList[0].leftImageUrl)
+    const [leftImageBackgroundColor, setLeftImageBackgroundColor] = useState(props.dataList[0].leftImageBackgroundColor)
+    const [itemTitle, setItemTitle] = useState(props.dataList[0].itemTitle)
+    const [itemDescription, setItemDescription] = useState(props.dataList[0].itemDescription)
+
+    const [rightSideText, setRightSideText] = useState(props.dataList[0].rightSideText)
+    const [rightSideTextColor, setRightSideTextColor] = useState(props.dataList[0].rightSideTextColor)
+
+    // useEffect(() => {
+    //     setHeading(props.heading)
+    //     setHeadingColor(props.headingColor)
+    //     setHeadingSize(props.headingSize)
+    //     setleftImageUrlFlag(props.leftImageUrlFlag)
+    //     setbelowLine()
+    // }[])
+
+    return (
+        <View>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    Alert.alert('Modal has been closed.');
+                    setModalVisible(!modalVisible);
+                }}>
+                <ScrollView>
+                    <View style={styles.centeredView}>
+                        <View style={styles.modalView}>
+                            <View style={styles.inputContainer}>
+
+                                <Text style={styles.element}>Parent Props</Text>
+
+                                <View style={styles.checkboxStyle}>
+                                    <TextInput
+                                        style={styles.input}
+                                        onChangeText={setHeading}
+                                        value={heading}
+                                        placeholder={heading}
+                                    />
+                                    <Text>
+                                        heading
+                                    </Text>
+                                </View>
+
+                                <View style={styles.checkboxStyle}>
+                                    <TextInput
+                                        style={styles.input}
+                                        onChangeText={setHeadingColor}
+                                        value={headingColor}
+                                        placeholder={headingColor}
+                                    />
+                                    <Text>
+                                        headingColor
+                                    </Text>
+                                </View>
+
+                                <View style={styles.checkboxStyle}>
+                                    <TextInput
+                                        style={styles.input}
+                                        onChangeText={setHeadingSize}
+                                        value={headingSize}
+                                        keyboardType="numeric"
+                                    />
+                                    <Text>
+                                        headingSize
+                                    </Text>
+                                </View>
+
+                                <View style={styles.checkboxStyle}>
+                                    <CheckBox
+                                        disabled={false}
+                                        value={leftImageUrlFlag}
+                                        onValueChange={() => {
+                                            setleftImageUrlFlag(!leftImageUrlFlag)
+                                            console.warn(leftImageUrlFlag)
+                                        }}
+                                    />
+                                    <Text onPress={() => {
+                                        setleftImageUrlFlag(!leftImageUrlFlag)
+                                        console.warn(leftImageUrlFlag)
+                                    }}>
+                                        leftImageUrlFlag
+                                    </Text>
+                                </View>
+
+                                <View style={styles.checkboxStyle}>
+                                    <CheckBox
+                                        disabled={false}
+                                        value={belowLine}
+                                        onValueChange={() => {
+                                            setbelowLine(!belowLine)
+                                            console.warn(belowLine)
+                                        }}
+                                    />
+                                    <Text onPress={() => {
+                                        setbelowLine(!belowLine)
+                                        console.warn(belowLine)
+                                    }}>
+                                        belowLine
+                                    </Text>
+                                </View>
+
+                                <View style={styles.checkboxStyle}>
+                                    <CheckBox
+                                        disabled={false}
+                                        value={itemBoxBorder}
+                                        onValueChange={() => {
+                                            setItemBoxBorder(!itemBoxBorder)
+                                            console.warn(itemBoxBorder)
+                                        }}
+                                    />
+                                    <Text onPress={() => {
+                                        setItemBoxBorder(!itemBoxBorder)
+                                        console.warn(itemBoxBorder)
+                                    }}>
+                                        itemBoxBorder
+                                    </Text>
+                                </View>
+
+                                <View style={styles.checkboxStyle}>
+                                    <CheckBox
+                                        disabled={false}
+                                        value={itemBoxBorderLeftHighlight}
+                                        onValueChange={() => {
+                                            setItemBoxBorderLeftHighlight(!itemBoxBorderLeftHighlight)
+                                            console.warn(itemBoxBorderLeftHighlight)
+                                        }}
+                                    />
+                                    <Text onPress={() => {
+                                        setItemBoxBorderLeftHighlight(!itemBoxBorderLeftHighlight)
+                                        console.warn(itemBoxBorderLeftHighlight)
+                                    }}>
+                                        itemBoxBorderLeftHighlight
+                                    </Text>
+                                </View>
+
+                                <View style={styles.checkboxStyle}>
+                                    <TextInput
+                                        style={styles.input}
+                                        onChangeText={setItemBoxBorderLeftHighlightColor}
+                                        value={itemBoxBorderLeftHighlightColor}
+                                        placeholder={itemBoxBorderLeftHighlightColor}
+                                    />
+                                    <Text>
+                                        itemBoxBorderLeftHighlightColor
+                                    </Text>
+                                </View>
+
+                                <View style={styles.checkboxStyle}>
+                                    <CheckBox
+                                        disabled={false}
+                                        value={rightArrowVisibility}
+                                        onValueChange={() => {
+                                            setRightArrowVisibility(!rightArrowVisibility)
+                                            console.warn(rightArrowVisibility)
+                                        }}
+                                    />
+                                    <Text onPress={() => {
+                                        setRightArrowVisibility(!rightArrowVisibility)
+                                        console.warn(rightArrowVisibility)
+                                    }}>
+                                        rightArrowVisibility
+                                    </Text>
+                                </View>
+
+                                <View style={styles.checkboxStyle}>
+                                    <CheckBox
+                                        disabled={false}
+                                        value={rightSideTextFlag}
+                                        onValueChange={() => {
+                                            setRightSideTextFlag(!rightSideTextFlag)
+                                            console.warn(rightSideTextFlag)
+                                        }}
+                                    />
+                                    <Text onPress={() => {
+                                        setRightSideTextFlag(!rightSideTextFlag)
+                                        console.warn(rightSideTextFlag)
+                                    }}>
+                                        rightSideTextFlag
+                                    </Text>
+                                </View>
+
+                                <View style={styles.checkboxStyle}>
+                                    <TextInput
+                                        style={styles.input}
+                                        onChangeText={setRightSideIcon}
+                                        value={rightSideIcon}
+                                        placeholder={rightSideIcon}
+                                    />
+                                    <Text>
+                                        rightSideIcon url
+                                    </Text>
+                                </View>
+
+                                <Text style={styles.element}>Item Props</Text>
+
+                                <View style={styles.checkboxStyle}>
+                                    <TextInput
+                                        style={styles.input}
+                                        onChangeText={setLeftImageUrl}
+                                        value={leftImageUrl}
+                                        placeholder={leftImageUrl}
+                                    />
+                                    <Text>
+                                        leftImageUrl url
+                                    </Text>
+                                </View>
+
+                                <View style={styles.checkboxStyle}>
+                                    <TextInput
+                                        style={styles.input}
+                                        onChangeText={setLeftImageBackgroundColor}
+                                        value={leftImageBackgroundColor}
+                                        placeholder={leftImageBackgroundColor}
+                                    />
+                                    <Text>
+                                        leftImageBackgroundColor
+                                    </Text>
+                                </View>
+
+                                <View style={styles.checkboxStyle}>
+                                    <TextInput
+                                        style={styles.input}
+                                        onChangeText={setItemTitle}
+                                        value={itemTitle}
+                                        placeholder={itemTitle}
+                                    />
+                                    <Text>
+                                        itemTitle
+                                    </Text>
+                                </View>
+
+                                <View style={styles.checkboxStyle}>
+                                    <TextInput
+                                        style={styles.input}
+                                        onChangeText={setItemDescription}
+                                        value={itemDescription}
+                                        placeholder={itemDescription}
+                                    />
+                                    <Text>
+                                        itemDescription
+                                    </Text>
+                                </View>
+
+                                <View style={styles.checkboxStyle}>
+                                    <TextInput
+                                        style={styles.input}
+                                        onChangeText={setRightSideText}
+                                        value={rightSideText}
+                                        placeholder={rightSideText}
+                                    />
+                                    <Text>
+                                        rightSideText
+                                    </Text>
+                                </View>
+
+                                <View style={styles.checkboxStyle}>
+                                    <TextInput
+                                        style={styles.input}
+                                        onChangeText={setRightSideTextColor}
+                                        value={rightSideTextColor}
+                                        placeholder={rightSideTextColor}
+                                    />
+                                    <Text>
+                                        rightSideTextColor
+                                    </Text>
+                                </View>
+                            </View>
+                            <Pressable
+                                style={[styles.button, styles.buttonClose]}
+                                onPress={() => {
+                                    setModalVisible(!modalVisible)
+                                    props.updateProps({
+                                        heading: heading,
+                                        headingColor: headingColor, 
+                                        headingSize: headingSize, 
+                                        leftImageUrlFlag: leftImageUrlFlag,
+                                        belowLine: belowLine,
+                                        itemBoxBorder: itemBoxBorder,
+                                        itemBoxBorderLeftHighlight: itemBoxBorderLeftHighlight,
+                                        itemBoxBorderLeftHighlightColor: itemBoxBorderLeftHighlightColor,
+                                        rightArrowVisibility: rightArrowVisibility,
+                                        rightSideTextFlag: rightSideTextFlag,
+                                        rightSideIcon: rightSideIcon,
+                                        dataList: props.dataList,
+                                        updateProps: props.updateProps
+                                    })
+                                }}>
+                                <Text style={styles.textStyle}>Update</Text>
+                            </Pressable>
+                        </View>
+                    </View>
+                </ScrollView>
+            </Modal>
+
+            <View style={ styles.btnContainer }>
+                <Pressable
+                    style={styles.btnStyle}
+                    onPress={() => setModalVisible(true)}
+                >
+                    <Text style={styles.textBtnStyle}>Update Props</Text>
+                </Pressable>
+            </View>
+
+        </View>
+    )
+}
+
+export default BottomSheet
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        margin: 20
+    },
+    checkboxStyle: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    input: {
+        height: 40,
+        marginRight: 12,
+        borderWidth: 1,
+        padding: 10,
+        borderRadius: 10,
+        marginBottom: 10,
+        borderColor: 'grey'
+    },
+    element: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 10,
+        marginTop: 10
+    },
+
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 35,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+    },
+    buttonOpen: {
+        backgroundColor: '#F194FF',
+    },
+    buttonClose: {
+        backgroundColor: '#2196F3',
+    },
+    textStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: 'center',
+    },
+    btnStyle:{
+        width: 200,
+        borderRadius: 200,
+        backgroundColor: '#3c2afa',
+        padding:10,
+        justifyContent: 'flex-end',
+        marginRight: -50
+    },
+    textBtnStyle: {
+        color: '#FFF',
+        fontSize: 17,
+        fontWeight: 'bold',
+        marginLeft: 10
+    },
+    btnContainer: {
+        flexDirection: 'row-reverse',
+        marginBottom: 30,
+        marginTop: 10
+    }
+})
